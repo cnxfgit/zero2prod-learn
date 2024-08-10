6e43d0728aa04b7095d01d77ca0792720a435cde
hlx <lingxu.he@santachi.com.cn>
2024-08-10 16:00:38 +0800

    env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
@@ -14,4 +17,5 @@ async fn main() -> std::io::Result<()> {
 
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
+
 }
