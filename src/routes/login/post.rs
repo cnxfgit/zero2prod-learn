57d192707b3fbab806cf6defd751cea3e8213588
hlx <1198816371@qq.com>
2025-01-06 10:50:42 +0800

    flash messages

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,8 @@
 use crate::routes::error_chain_fmt;
 use actix_web::{
-    cookie::Cookie, error::InternalError, HttpResponse, web
+    error::InternalError, HttpResponse, web
 };
+use actix_web_flash_messages::FlashMessage;
 use reqwest::header::LOCATION;
 use secrecy::Secret;
 use sqlx::PgPool;
@@ -40,9 +41,9 @@ pub async fn login(
                 AuthError::InvaildCredentials(_) => LoginError::AuthError(e.into()),
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
             };
+            FlashMessage::error(e.to_string()).send();
             let response = HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/login"))
-                .cookie(Cookie::new("_flash", e.to_string()))
                 .finish();
             Err(InternalError::from_response(e, response))
         }
--- src/startup.rs
+++ src/startup.rs
@@ -5,12 +5,19 @@ use crate::email_client::EmailClient;
 use crate::routes::{
     confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
+use actix_web::{
+    cookie::Key,
+    dev::Server,
+    web::{self, Data},
+    App, HttpServer,
+};
+use actix_web_flash_messages::storage::CookieMessageStore;
+use actix_web_flash_messages::FlashMessagesFramework;
 use secrecy::Secret;
-use actix_web::dev::Server;
-use actix_web::{web::{self, Data}, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use tracing_actix_web::TracingLogger;
+use secrecy::ExposeSecret;
 
 pub struct Application {
     port: u16,
@@ -75,8 +82,12 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let email_client = web::Data::new(email_client);
     let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let message_store =
+        CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(message_framework.clone())
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
@@ -95,7 +106,6 @@ pub fn run(
 
     Ok(server)
 }
-
 
 #[derive(Clone)]
 pub struct HmacSecret(pub Secret<String>);
